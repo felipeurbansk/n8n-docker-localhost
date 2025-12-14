456fb90d721e65020b6f168fbde0721d675dd433
Felipe Urbanski <felipeurbansk@gmail.com>
2025-12-14 13:45:00 -0300

    feat: remove coolify dependency

--- coolify-base/resources/views/components/server/sidebar-security.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="flex flex-col items-start gap-2 min-w-fit">
-    <a class="{{ request()->routeIs('server.security.patches') ? 'menu-item menu-item-active' : 'menu-item' }}"
-        href="{{ route('server.security.patches', $parameters) }}">
-        Server Patching
-    </a>
-</div>
